07cff078b1df8d1292daf1abf69a62b0aa86dcc9
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-09-25 23:51:57 +0100

    Initial Commit

---
+++ Cook/CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookApp.swift
+//  Cook
+//
+//  Created by Jack Finnis on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
